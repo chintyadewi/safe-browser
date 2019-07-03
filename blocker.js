74be6943a3e90a01d834a5e702540595fef80f21
chintyadewi <chintyadewi437@gmail.com>
2019-07-03 22:09:50 +0700

    Get & Post data dari API

--- blocker.js
+++ blocker.js
@@ -1,4 +1,8 @@
+// const fetch = require("node-fetch");
+
 var url = window.location.href;
+var titlePage = document.title;
+
 var today = new Date();
 var date =
   today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
@@ -11,5 +15,34 @@ if (url == "https://www.wikipedia.org/") {
   url = "https://www.mozilla.org/en-US";
   window.location = url;
 } else {
-  console.log(url, date + " " + time);
+  var url = "http://jsonplaceholder.typicode.com/posts";
+
+  // Get Data
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => console.error(error));
+
+  var dataPost = {
+    userId: 10,
+    id: 101,
+    title: "lkjklsjdlks",
+    body: "lkjkjdskfjk"
+  };
+
+  // Post Data
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(dataPost),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(response => console.log("Success:", JSON.stringify(response)))
+    .catch(error => console.error("Error:", error));
+
+  console.log(url, date + " " + time, titlePage);
 }
