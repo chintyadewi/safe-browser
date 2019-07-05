54581768807a51dbf715140cccdca682c473c9c2
chintyadewi <chintyadewi437@gmail.com>
2019-07-06 00:50:15 +0700

    edit alamat API menjadi backend safebrowser

--- blocker.js
+++ blocker.js
@@ -1,6 +1,4 @@
-// const fetch = require("node-fetch");
-
-var url = window.location.href;
+var urlAccess = window.location.href;
 var titlePage = document.title;
 
 var today = new Date();
@@ -9,30 +7,20 @@ var date =
 var time =
   today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
-if (url == "https://www.wikipedia.org/") {
-  console.log(url, date + " " + time);
+if (urlAccess == "https://www.wikipedia.org/") {
+  console.log(urlAccess, date + " " + time);
   // change url
-  url = "https://www.mozilla.org/en-US";
-  window.location = url;
+  // url = "https://www.mozilla.org/en-US";
+  // window.location = url;
 } else {
-  var apiUrl = "http://jsonplaceholder.typicode.com/posts";
-
-  // Get Data
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => console.error(error));
-
-  var dataPost = {
-    userId: 10,
-    id: 101,
-    title: "lkjklsjdlks",
-    body: "lkjkjdskfjk"
-  };
+  var apiUrl = "https://safebrowser.herokuapp.com/history";
 
   // Post Data
+  var dataPost = {
+    title: titlePage,
+    url: urlAccess
+  };
+
   fetch(apiUrl, {
     method: "POST",
     body: JSON.stringify(dataPost),
@@ -44,5 +32,11 @@ if (url == "https://www.wikipedia.org/") {
     .then(response => console.log("Success:", JSON.stringify(response)))
     .catch(error => console.error("Error:", error));
 
-  console.log(url, date + " " + time, titlePage);
+  // Get Data
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => console.error(error));
 }
